4f2239509b4994f4a38360e4d37072cd83853145
marco00petrucci <marco00petrucci@gmail.com>
2022-06-29 08:50:01 +0200

    first commit

---
+++ header-opensidenav.js
@@ -0,0 +1,52 @@
+//Menu dinamico allo scroll
+function gestoreScroll() {
+    if (window.matchMedia("(max-width: 800px)").matches) {
+        document.getElementById("titolo").style.display = "block";
+        document.getElementById("gif").style.display = "none";
+        document.getElementById("menu").style.display = "block";
+        document.getElementById("cercaHeader").style.display = "none";
+        document.getElementById("header").style.padding = "2% 0 0";
+        document.getElementById("menu").style.padding = "0% 6%";
+    }
+    if ((window.matchMedia("(min-width: 800px)").matches) && (document.documentElement.scrollTop > 80)) {
+        document.getElementById("titolo").style.display = "none";
+        document.getElementById("gif").style.display = "inline-block";
+        document.getElementById("menu").style.display = "inline";
+        document.getElementById("menu").style.padding = "0% 6%";
+        document.getElementById("header").style.padding = "1%";
+        document.getElementById("cercaHeader").style.display = "inline-block";
+    }
+    if ((window.matchMedia("(min-width: 800px)").matches) && (document.documentElement.scrollTop < 80)) {
+        document.getElementById("titolo").style.display = "block";
+        document.getElementById("gif").style.display = "none";
+        document.getElementById("menu").style.display = "block";
+        document.getElementById("cercaHeader").style.display = "none";
+        document.getElementById("header").style.padding = "2%";
+        document.getElementById("menu").style.padding = "0% 6%";
+    }
+    scrollFunction();
+}
+
+//Sidenav Search
+function gestoreOpenCloseNav() {
+    var nodocercaHeader = document.getElementById("cercaHeader");
+    nodocercaHeader.onclick = openNav;
+    var nodocloseBtn = document.getElementById("closeBtn-icon");
+    nodocloseBtn.onclick = closeNav;
+}
+
+var nodoTestoCerca, nodoCercaBtn, nodoRisultato, buttonTop;
+
+function gestoreLoadCercaTop() {
+    buttonTop = document.getElementById("buttonTop");
+    buttonTop.onclick = topFunction;
+    nodoTestoCerca = document.getElementById("sideTestoCerca");
+    nodoCercaBtn = document.getElementById("sideCercaBtn");
+    nodoRisultato = document.getElementById("risultato");
+    nodoCercaBtn.onclick = gestoreCerca;
+    gestoreOpenCloseNav();
+}
+
+window.onload = gestoreLoadCercaTop;
+
+window.onscroll = gestoreScroll;
